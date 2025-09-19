f0aedb90129a8f55af1ec5f263c1a9cc35e74869
Alexander Heffernan <heffernan.alexander@icloud.com>
2025-09-19 19:46:30 +1200

    Init commit

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="navbar">
+        <h1 class="gradient-text-effect">Rally</h1>
+    </div>
+</template>
+
+<style scoped>
+.navbar {
+    background-color: white;
+    padding: 20px;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="home">
+        <h1>Welcome Back! 👋</h1>
+        <p>You have x meets coming up and y meets waiting for your response.</p>
+        <div class="main-container">
+            <div class="main-panel">
+                <h2>Upcoming Meets</h2>
+                <h2>Action Required</h2>
+            </div>
+            <div class="side-panel">
+                <h3>My Groups</h3>
+                <h3>Quick Actions</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.home {
+    padding: 20px;
+}
+
+.main-container {
+    display: flex;
+    gap: 20px;
+}
+
+.main-panel {
+    flex: 3;
+    border-radius: 8px;
+}
+
+.side-panel {
+    flex: 1;
+    border-radius: 8px;
+}
+
+</style>
